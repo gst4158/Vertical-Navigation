b9cdd70b7af7f0a9b33d41c2396775333a5e7cce
Gregory Thomason <gregory_thomason@hotmail.com>
2015-04-23 11:18:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-	
 	$('.cover:first').addClass('first-child');
 	$('.cover:last').addClass('last-child');
 	$(".next, .prev").on("click", function(e) {
-	    vertNav($(this));
-	});
-
-	function vertNav(clicked) {
 		
 		// the container that will be scrolled to
 		var target = '.cover';
-	    container = $(clicked).parent();
+	    container = $(this).parent();
 
 	    // am I the last .container in my group?
 	    while (document != container[0] && container.find('~'+target, '~:has('+target+')').length == 0)
@@ -19,31 +14,30 @@
 	    nextdiv = container.nextAll(target, ':has('+target+')').first();
 	    
 	    // if clicked next object
-	    if ( $(clicked).hasClass('next') ) {
+	    if ( $(this).hasClass('next') ) {
 	    	// no next .container found, go back to first container
 	    	if (nextdiv.length==0) nextdiv = $(document).find(target+':first');
 	    
 	    	//$(document).scrollTop(nextdiv.offset().top);
-	    	$('html').animate({scrollTop:nextdiv.offset().top},300);	    
+	    	$('body, html').animate({scrollTop:nextdiv.offset().top},300);	    
 	    }
 	
 		// if clicked prev
-	    if ( $(clicked).hasClass('prev') ) {
+	    if ( $(this).hasClass('prev') ) {
 	    	// no next .container found, go back to first container
 
-	
 	    	// scroll to previous element
-	    	prevdiv = $(clicked).parents('.cover').prev('.cover');
+	    	prevdiv = $(this).parents('.cover').prev('.cover');
 	    	
 	    	// if is first element on page, then scroll to last element
-	    	if ( $(clicked).parents('.cover').hasClass('first-child') ) {
+	    	if ( $(this).parents('.cover').hasClass('first-child') ) {
 		    	prevdiv =  $(document).find(target+':last');
 	    	};    	
 	    	
 	    	//$(document).scrollTop(nextdiv.offset().top);
-	    	$('html').animate({scrollTop:prevdiv.offset().top},300);	    
+	    	$('body, html').animate({scrollTop:prevdiv.offset().top},300);	    
 	    }
 	    
 	    // $(this).parent().next() // this is the next div container.
 	    return false;
-	}	
+	});
